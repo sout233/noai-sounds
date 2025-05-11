329f0ef12c62e3a490fc2c75ea7392c99b8e6054
sout_Nantang <55623894+sout233@users.noreply.github.com>
2025-05-11 08:29:18 +0800

    init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import Iridescence from "../../components/Iridescence";
+
+export default async function Page() {
+  return (
+    <ul>
+      <h1> hi</h1>
+      <Iridescence
+
+  color={[1, 1, 1]}
+
+  mouseReact={false}
+
+  amplitude={0.1}
+
+  speed={1.0}
+
+/>
+    </ul>
+  );
+}
